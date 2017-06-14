987d6b37eafbda3d16db6a40c792cac5a6a4c414
dd <dd>
2017-06-14 15:10:02 +0800

    格式化目录

---
+++ app/src/main/java/com/nzsc/cmakeandtest/Activity/MainActivity.java
@@ -0,0 +1,104 @@
+package com.nzsc.cmakeandtest.Activity;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+
+import com.nzsc.cmakeandtest.Adapter.ExamplePagerAdapter;
+
+import com.nzsc.cmakeandtest.View.ScaleTransitionPagerTitleView;
+
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.UIUtil;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.SimplePagerTitleView;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String[] CHANNELS = new String[]{"KITKAT", "NOUGAT", "DONUT"};
+    private List<String> mDataList = Arrays.asList(CHANNELS);
+    private ExamplePagerAdapter mExamplePagerAdapter = new ExamplePagerAdapter(mDataList);
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mViewPager.setAdapter(mExamplePagerAdapter);
+
+
+        initMagicIndicator2();
+
+    }
+
+    private void initMagicIndicator2() {
+        MagicIndicator magicIndicator = (MagicIndicator) findViewById(R.id.magic_indicator2);
+        magicIndicator.setBackgroundColor(Color.WHITE);
+        CommonNavigator commonNavigator = new CommonNavigator(this);
+        commonNavigator.setAdjustMode(true);
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
+            @Override
+            public int getCount() {
+                return mDataList == null ? 0 : mDataList.size();
+            }
+
+            @Override
+            public IPagerTitleView getTitleView(Context context, final int index) {
+                SimplePagerTitleView simplePagerTitleView = new ScaleTransitionPagerTitleView(context);
+                simplePagerTitleView.setText(mDataList.get(index));
+                simplePagerTitleView.setTextSize(18);
+                simplePagerTitleView.setNormalColor(Color.parseColor("#616161"));
+                simplePagerTitleView.setSelectedColor(Color.parseColor("#f57c00"));
+                simplePagerTitleView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mViewPager.setCurrentItem(index);
+                    }
+                });
+                return simplePagerTitleView;
+            }
+
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                LinePagerIndicator indicator = new LinePagerIndicator(context);
+                indicator.setStartInterpolator(new AccelerateInterpolator());
+                indicator.setEndInterpolator(new DecelerateInterpolator(1.6f));
+                indicator.setYOffset(UIUtil.dip2px(context, 39));
+                indicator.setLineHeight(UIUtil.dip2px(context, 1));
+                indicator.setColors(Color.parseColor("#f57c00"));
+                return indicator;
+            }
+
+            @Override
+            public float getTitleWeight(Context context, int index) {
+                if (index == 0) {
+                    return 2.0f;
+                } else if (index == 1) {
+                    return 1.2f;
+                } else {
+                    return 1.0f;
+                }
+            }
+        });
+        magicIndicator.setNavigator(commonNavigator);
+        ViewPagerHelper.bind(magicIndicator, mViewPager);
+    }
+
+
+}
---
+++ app/src/main/java/com/nzsc/cmakeandtest/MySerial/Serial.java
@@ -0,0 +1,14 @@
+package com.nzsc.cmakeandtest.MySerial;
+
+/**
+ * Created by 夜墨 on 2017/6/14.
+ *
+ */
+
+public class Serial {
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
